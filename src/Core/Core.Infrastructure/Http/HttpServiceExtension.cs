747728a668aa8b35fd8f5d65872625fee2768cf2
luuthuong <luuthuong20022000@gmail.com>
2025-03-04 21:37:39 +0700

    feat: update core http client request handler

--- src/Core/Core.Infrastructure/Http/HttpServiceExtension.cs
+++ src/Core/Core.Infrastructure/Http/HttpServiceExtension.cs
@@ -15,16 +15,45 @@ public static class HttpServiceExtension
         if (tokenIssuerSettings is null)
             throw new ArgumentNullException("Token Issuer wasn't found.");
 
-        var integrationHttpSettings = configuration.GetSection("IntegrationHttpSettings") ?? throw new ArgumentNullException("IntergrationHttpSettings wasn't found.");
+        var integrationHttpSettings = configuration.GetSection("IntegrationHttpSettings") ??
+                                      throw new ArgumentNullException("IntergrationHttpSettings wasn't found.");
 
         services.Configure<TokenIssuerSettings>(tokenIssuerSettings);
         services.Configure<IntegrationHttpSetting>(integrationHttpSettings);
 
         services.AddHttpClient();
 
-        services.AddTransient<ITokenService, TokenService>();
-        services.AddTransient<IHttpRequest, HttpRequestHandler>();
+        services.AddHttpClient(HttpClientNames.Default);
 
+        services.AddHttpClient(HttpClientNames.ApiGateWay, client =>
+        {
+            var uriString = configuration.GetSection("ApiGatewayAddress").Value;
+            if (uriString != null)
+                client.BaseAddress = new System.Uri(uriString);
+        });
+
+        services.AddHttpClient(HttpClientNames.Authority, client =>
+        {
+            var uriString = configuration.GetSection("TokenIssuerSettings:Authority").Value;
+            if (uriString != null)
+                client.BaseAddress = new System.Uri(uriString);
+        });
+
+        services.AddKeyedSingleton<IHttpRequest, HttpRequestHandler>(
+            HttpClientNames.Default,
+            (sp, _) => new HttpRequestHandler(sp.GetRequiredService<IHttpClientFactory>(), HttpClientNames.Default));
+
+        services.AddKeyedSingleton<IHttpRequest, HttpRequestHandler>(
+            HttpClientNames.ApiGateWay,
+            (sp, _) => new HttpRequestHandler(sp.GetRequiredService<IHttpClientFactory>(), HttpClientNames.ApiGateWay)
+        );
+
+        services.AddKeyedSingleton<IHttpRequest, HttpRequestHandler>(
+            HttpClientNames.Authority,
+            (sp, _) => new HttpRequestHandler(sp.GetRequiredService<IHttpClientFactory>(), HttpClientNames.Authority)
+        );
+
+        services.AddTransient<ITokenService, TokenService>();
         return services;
     }
-} 
+}
